fc79683a563273e82f895b3c635c391ed3741a92
AsmaSami <asma.sami8989@gmail.com>
2023-03-31 21:29:01 -0700

    update

--- 0x02-functions_nested_loops/main.c
+++ 0x02-functions_nested_loops/main.c
@@ -1,17 +1,12 @@
 #include "main.h"
+
 /**
- *  main - prints the strings "Holbertson" from a character array.
- *  Return: 0 on success.
+ * main - check the code
+ *
+ * Return: Always 0.
  */
 int main(void)
 {
-	char c[] = "main";
-	int i;
-
-	for (i = 0; i < 9; i++)
-        {
-		_putchar(c[i]);
-	}
-        _putchar('\n');
-	return (0);
+    print_alphabet();
+    return (0);
 }
